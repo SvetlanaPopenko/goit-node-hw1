e1d5dec69a39297c52153b2fc350304a4827110a
SvetlanaPopenko <spopenko02@gmail.com>
2023-04-05 01:53:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+          const allContacts = await listContacts();
+          console.table(allContacts);
+      break;
+
+    case "get":
+          const contact = await getContactById(id);
+          console.log(contact);
+      break;
+
+    case "add":
+          const newContact = await addContact({name, email, phone});
+          console.log(newContact);
+      break;
+
+    case "remove":
+          const newContacts = await removeContact(id);
+          console.log(newContacts);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
